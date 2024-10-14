797dddd76f070ba906e1b297b6cd307790445968
Robin Sträng <91435503+Choppelito@users.noreply.github.com>
2024-10-14 13:34:45 +0200

    Basic tructure for class Person and Item done

--- TjuvOchPolis/TjuvOchPolis/obj/Debug/net8.0/TjuvOchPolis.AssemblyInfo.cs
+++ TjuvOchPolis/TjuvOchPolis/obj/Debug/net8.0/TjuvOchPolis.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TjuvOchPolis")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c65cc4c25859d93cec5312bad77eeea6a4948e7e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TjuvOchPolis")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TjuvOchPolis")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
